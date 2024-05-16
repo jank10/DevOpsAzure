6d664ceea4dc72f444f418691f067f196324bbee
jank10 <jank10@gmx.net>
2024-05-16 22:11:28 +0200

    Erste Änderung am Code um Github Actions zu testen

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('!!! Hello DevOps FS2024!!!');
+  res.send('!!! Hello DevOps FS2024. Jan Küchli hat eine Änderung vorgenommen.!!!');
 });
 
 app.listen(PORT, HOST);
